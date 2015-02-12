32d2de62dbac8487af1484cf03ab09ab6b8a8c69
thatspaceygamer <thatspacegamer@gmail.com>
2015-02-12 10:55:33 -0800

    Created by Coding Ground Client

---
+++ New Project/Newfile.rb
@@ -0,0 +1,19 @@
+class hash
+    dbase_01 = {
+        "Soren" => "1234",
+        "Reina" => "5678",
+        "Gary" => "1111",
+        "Robin" => "9999"
+    }
+    dbase_02 = {
+        "b-day" => "8/24/01",
+		"age" => "13",
+		"enjoys" => ["Computer", "Reading", "Programing (Duh)", "Playing larinet"],
+		"computers" => ["Raspberry Pi", "Samsung ATIV Book 4 with modified I5 processor with 16 gb/ram."],
+		"screen" => ["32-inch flatscreen", "14-inch laptop monitor"],
+		"friends" => ["Jared Chen", "Jared Goddard", "Christopher Rose", "Caleb Mark", "His Majesty the Artifical Intellegence he accidenlt created and gave too much power."],
+		"steam" => "ThatSpaceyGamer", 
+		"webfriends" => ["Fluffy the Cat", "Stargazer45", "Big G", "Spunklizard", "And some more!"],
+		"help" => ["b-day", "age", "enjoys", "computers", "screen", "friends", "steam", "webfriends", "help"]
+    }
+end
